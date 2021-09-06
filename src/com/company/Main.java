ededda2fad8fa603f8b7848a3723b27cbdf81fe6
ivanmf42 <ivan.martinez.9411@gmail.com>
2021-09-06 04:53:52 -0500

    Algoritmo Binario a Decimal, se genera csv

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,57 @@
 package com.company;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+
+        FileWriter csvWriter = new FileWriter("new.csv");
+        csvWriter.append("T(n)");
+        csvWriter.append(",");
+        csvWriter.append("N");
+        csvWriter.append("\n");
+
+        int j;
+        for(j=0; j<15; j++){
+            int i, aleatorio, cont=0;
+            int n= j*2;//Integer.parseInt(System.console().readLine());
+            //Creamos y llenamos los arreglos de manera aleatoria
+            int[] A = new int[n], B = new int [n], C =new int[n];
+            for (i=0; i<n; i++){
+                if(Math.random()>0.5){
+                    aleatorio=1;
+                }else {
+                    aleatorio=0;
+                }
+                A[i]= aleatorio;
+            }
+            for (i=0; i<n; i++){
+                if(Math.random()>0.5){
+                    aleatorio=1;
+                }else {
+                    aleatorio=0;
+                }
+                B[i]= aleatorio;
+            }
+            //Convertimos el binario a decimal
+            int decimal=0, exponente=n-1;
+            for(i=0;i<n;i++){
+                C[i]= A[i] & B[i];
+                decimal += (C[i] * (int) Math.pow(2,exponente));
+                exponente--;
+            }
+            System.out.println("Decimal es " + decimal);
+
+            csvWriter.append(String.valueOf(cont));
+            csvWriter.append(",");
+            csvWriter.append(String.valueOf(n));
+            csvWriter.append("\n");
+
+            csvWriter.flush();
+        }
+        csvWriter.close();
+
     }
 }
